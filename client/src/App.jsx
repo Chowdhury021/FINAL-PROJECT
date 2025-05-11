e019033c9980da0fdca5e5b2aa8833fea1c1fd5d
Jordan Carcamo <jordancarcamo14@yahoo.com>
2025-05-11 18:04:43 -0400

    private policy updated.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,6 +10,8 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import Verify from "./pages/Verify/Verify";
 import MyOrders from "./pages/MyOrders/MyOrders";
+import PrivacyPolicy from "./components/pages/PrivacyPolicy";
+
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
@@ -25,6 +27,7 @@ const App = () => {
           <Route path="/order" element={<PlaceOrder />} />
           <Route path="/verify" element={<Verify />} />
           <Route path="/myorders" element={<MyOrders />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         </Routes>
       </div>
       <Footer />

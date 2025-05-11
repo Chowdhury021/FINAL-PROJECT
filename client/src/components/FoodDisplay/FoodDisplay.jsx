426206c870f68b3e900a07c483c306c95f983648
Fazle Rayhan <faz@fazles-air.myfiosgateway.com>
2025-05-11 03:04:16 -0400

    Client Working succesfully

--- client/src/components/FoodDisplay/FoodDisplay.jsx
+++ client/src/components/FoodDisplay/FoodDisplay.jsx
@@ -1,10 +1,10 @@
 import React, { useContext } from "react";
 import "./FoodDisplay.css";
-import { storeContext } from "../../context/StoreContext";
+import { StoreContext } from "../../context/StoreContext";
 import FoodItem from "../FoodItem/FoodItem";
 
 const FoodDisplay = ({ category }) => {
-  const { food_list } = useContext(storeContext);
+  const { food_list } = useContext(StoreContext);
   return (
     <div className="food-display" id="food-display">
       <h2>Top dishes near you</h2>

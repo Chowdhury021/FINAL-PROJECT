e465624395fb80f1a0d03ad87931c90924ea3c84
Chris-Dot1 <smfoster52@gmail.com>
2025-05-11 13:13:49 -0400

    added menu redirection on icon

--- client/src/components/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import "./ExploreMenu.css";
 import { menu_list } from '../../assets/frontend_assets/assets.js';
 
-const ExploreMenu = ({category,setCategory}) => {
+const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className="explore-menu" id="explore-menu">
+    <div className="explore-menu" id="menu"> {/* <-- changed id from 'explore-menu' to 'menu' */}
       <h1>Explore our menu</h1>
       <p className="explore-menu-text">
         Choose from a diverse menu featuring a detectable array of dishes. Our
@@ -14,16 +14,25 @@ const ExploreMenu = ({category,setCategory}) => {
       <div className="explore-menu-list">
         {menu_list.map((item, index) => {
           return (
-            <div onClick={()=>setCategory(prev=>prev===item.menu_image.name?"All":item.menu_name)} key={index} className="explore-menu-list-item">
-              <img className={category===item.menu_name?"active":""} src={item.menu_image} alt="" />
+            <div
+              onClick={() => setCategory(prev => prev === item.menu_image.name ? "All" : item.menu_name)}
+              key={index}
+              className="explore-menu-list-item"
+            >
+              <img
+                className={category === item.menu_name ? "active" : ""}
+                src={item.menu_image}
+                alt={item.menu_name}
+              />
               <p>{item.menu_name}</p>
             </div>
           );
         })}
       </div>
-      <hr/>
+      <hr />
     </div>
   );
 };
 
 export default ExploreMenu;
+

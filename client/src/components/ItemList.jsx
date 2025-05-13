1946010a73bffc468527d50885749587b428db05
PapiP3 <164602532+PapiP3@users.noreply.github.com>
2025-05-13 04:06:58 -0400

    Use ItemList in FoodDisplay and wire up addToCart

---
+++ client/src/components/ItemList.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import DishCard from './DishCard';
+
+export default function ItemList({ items, onAdd }) {
+    if (items.length === 0) {
+        return <div className="no-items">No items found in this category.</div>;
+    }
+
+  return (
+    <div className="food-display-list">
+      {items.map(item => (
+        <DishCard key={item._id} dish={item} onAdd={onAdd} />
+      ))}
+    </div>
+  );
+}

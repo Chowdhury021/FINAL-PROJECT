844781a15190a6aab57ebca473de78bc50dc2ca6
Chris-Dot1 <smfoster52@gmail.com>
2025-05-15 14:52:20 -0400

    fixed menu button

--- client/src/components/ExploreMenu/ExploreMenu.jsx
+++ client/src/components/ExploreMenu/ExploreMenu.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "./ExploreMenu.css";
-import { menu_list } from '../../assets/frontend_assets/assets.js';
+import { menu_list } from "../../assets/frontend_assets/assets.js";
 
 const ExploreMenu = ({ category, setCategory }) => {
   return (
-    <div className="explore-menu" id="menu"> {/* <-- changed id from 'explore-menu' to 'menu' */}
+    <div className="explore-menu" id="explore-menu">
       <h1>Explore our menu</h1>
       <p className="explore-menu-text">
-        Choose from a diverse menu featuring a detectable array of dishes. Our
+        Choose from a diverse menu featuring a delectable array of dishes. Our
         mission is to satisfy your cravings and elevate your dining experience,
         one delicious meal at a time.
       </p>
@@ -15,8 +15,12 @@ const ExploreMenu = ({ category, setCategory }) => {
         {menu_list.map((item, index) => {
           return (
             <div
-              onClick={() => setCategory(prev => prev === item.menu_image.name ? "All" : item.menu_name)}
               key={index}
+              onClick={() =>
+                setCategory((prev) =>
+                  prev === item.menu_name ? "All" : item.menu_name
+                )
+              }
               className="explore-menu-list-item"
             >
               <img
@@ -35,4 +39,5 @@ const ExploreMenu = ({ category, setCategory }) => {
 };
 
 export default ExploreMenu;
+
 

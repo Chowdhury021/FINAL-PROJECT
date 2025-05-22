4636830b84cdb4efdd054601eb0447caf9287689
Dev.BPerez <164602532+PapiP3@users.noreply.github.com>
2025-05-21 23:34:47 -0400

    Align page imports with actual folder/file casing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,20 @@ import React, { useState } from "react";
 //import Navbar from "./components/Navbar/Navbar";
 import Navbar from "./components/navbar/navbar.jsx";
 import { Route, Routes } from "react-router-dom";
-import Home from "./pages/Home/Home";
-import Cart from "./pages/Cart/Cart";
-import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+//import Home from "./pages/Home/Home";
+import Home from "./pages/home/home.jsx";
+//import Cart from "./pages/Cart/Cart";
+import Cart from "./pages/cart/cart.jsx";
+//import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
+import PlaceOrder from "./pages/placeOrder/placeOrder.jsx";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Verify from "./pages/Verify/Verify";
-import MyOrders from "./pages/MyOrders/MyOrders";
+//import Verify from "./pages/Verify/Verify";
+import Verify from "./pages/Verify/Verify.jsx"; // case matches folder & file
+//import MyOrders from "./pages/MyOrders/MyOrders";
+import MyOrders from "./pages/MyOrders/MyOrders.jsx"; // case matches folder & file
 import PrivacyPolicy from "./components/pages/PrivacyPolicy";
 import AboutUs from "./components/pages/About";
 import Delivery from "./components/pages/Delivery.jsx";

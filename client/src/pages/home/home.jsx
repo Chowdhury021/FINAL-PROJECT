fb57d834be8d1c2f74da6c343f715a1210bc7c71
Dev.BPerez <164602532+PapiP3@users.noreply.github.com>
2025-05-23 00:56:46 -0400

    fix: correct Header import path casing in home.jsx

--- client/src/pages/home/home.jsx
+++ client/src/pages/home/home.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from 'react'
 //import './Home.css'
 import "./home.css";
-import Header from '../../components/Header/Header'
+//import Header from '../../components/Header/Header'
+import Header from "../../components/header/header";
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay'
 import AppDownload from '../../components/AppDownload/AppDownload'

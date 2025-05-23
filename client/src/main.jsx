c4a749c343439330b15b8c316b828d4fb55fae18
Dev.BPerez <164602532+PapiP3@users.noreply.github.com>
2025-05-22 23:01:54 -0400

    Wrap App in BrowserRouter to fix useNavigate context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,14 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+//import { BrowserRouter } from "react-router-dom";
+//import StoreContextProvider from './context/StoreContext.jsx';
 import { BrowserRouter } from "react-router-dom";
 import StoreContextProvider from './context/StoreContext.jsx';
 
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <StoreContextProvider>
-      <App />
-    </StoreContextProvider>
+        <BrowserRouter>
+     <StoreContextProvider>
+        <App />
+      </StoreContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
